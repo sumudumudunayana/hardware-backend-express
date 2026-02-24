82d9bca880cf09dd6b0f9980e6ba6d99d793a38b
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-02-24 18:19:19 +0530

    create category added

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -25,6 +25,16 @@ const getCategoryById = async (req, res) => {
   }
 };
 
+// CREATE CATEGORY
+const createCategory = async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(201).json(newCategory);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getCategories,
   getCategoryById,
