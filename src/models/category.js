c18aaa1ca17ae1fb46533f1f95d042426ba572cb
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-02-24 18:07:51 +0530

    model added

---
+++ src/models/category.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+  {
+    categoryName: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true
+    },
+    categoryDescription: {
+      type: String,
+      trim: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Category", categorySchema);
