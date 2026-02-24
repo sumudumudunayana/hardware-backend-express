d9ea3714532f3bea208599b314d300cd41647020
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-02-24 18:08:02 +0530

    routes are created

---
+++ src/routes/categoryRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getCategories,
+  getCategoryById,
+  createCategory,
+  updateCategory,
+  deleteCategory
+} = require("../controllers/categoryController");
+
+router.get("/", getCategories);
+router.get("/:id", getCategoryById);
+router.post("/", createCategory);
+router.put("/:id", updateCategory);
+router.delete("/:id", deleteCategory);
+
+module.exports = router;
