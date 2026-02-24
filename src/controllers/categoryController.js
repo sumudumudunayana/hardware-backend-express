0a474c9ee188f7d96be812e0f7d4578545f64d09
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-02-24 18:18:49 +0530

    get a single categories added

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -10,6 +10,21 @@ const getCategories = async (req, res) => {
   }
 };
 
+// GET SINGLE CATEGORY
+const getCategoryById = async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    res.status(200).json(category);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getCategories,
   getCategoryById,
