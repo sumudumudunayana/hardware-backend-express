2f2be0e0db7cda5de39ceb9b88e04ff3be42543c
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-02-24 18:20:05 +0530

    delete category added

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -54,6 +54,21 @@ const updateCategory = async (req, res) => {
   }
 };
 
+// DELETE CATEGORY
+const deleteCategory = async (req, res) => {
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(req.params.id);
+
+    if (!deletedCategory) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    res.status(200).json({ message: "Category deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getCategories,
   getCategoryById,
