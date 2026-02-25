decbec2ea1b973b3759e6a69848f6ea987062381
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-02-25 12:48:20 +0530

    added the auto incriment part

--- src/models/category.js
+++ src/models/category.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
+const Counter = require("./counter");
 
 const categorySchema = new mongoose.Schema(
   {
+    categoryId: {
+      type: Number,
+      unique: true
+    },
     categoryName: {
       type: String,
       required: true,
@@ -17,5 +22,18 @@ const categorySchema = new mongoose.Schema(
     timestamps: true
   }
 );
+
+// SAFE AUTO-INCREMENT
+categorySchema.pre("save", async function () {
+  if (this.isNew) {
+    const counter = await Counter.findOneAndUpdate(
+      { _id: "categoryId" },
+      { $inc: { sequenceValue: 1 } },
+      { returnDocument: "after", upsert: true }
+    );
+
+    this.categoryId = counter.sequenceValue;
+  }
+});
 
 module.exports = mongoose.model("Category", categorySchema);
