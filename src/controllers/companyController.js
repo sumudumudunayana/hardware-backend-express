b1e2c34b269af516773b78d1dffb99e9e9000faf
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-02-26 11:32:03 +0530

    get one method added

--- src/controllers/companyController.js
+++ src/controllers/companyController.js
@@ -10,6 +10,20 @@ const getCompanies = async (req, res) => {
   }
 };
 
+// GET ONE
+const getCompanyById = async (req, res) => {
+  try {
+    const company = await Company.findById(req.params.id);
+
+    if (!company) {
+      return res.status(404).json({ message: "Company not found" });
+    }
+
+    res.status(200).json(company);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 
 module.exports = {
