9760f3c29c7412e541ef1ec54d1518b080ad08b3
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-02-26 11:34:45 +0530

    delete method added

--- src/controllers/companyController.js
+++ src/controllers/companyController.js
@@ -54,6 +54,21 @@ const updateCompany = async (req, res) => {
   }
 };
 
+// DELETE
+const deleteCompany = async (req, res) => {
+  try {
+    const deletedCompany = await Company.findByIdAndDelete(req.params.id);
+
+    if (!deletedCompany) {
+      return res.status(404).json({ message: "Company not found" });
+    }
+
+    res.status(200).json({ message: "Company deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 
 module.exports = {
   getCompanies,
