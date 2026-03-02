202ee45962f82294e02299ebee0f24e02479c42f
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-02 14:18:22 +0530

    delete method added

--- src/controllers/distributorController.js
+++ src/controllers/distributorController.js
@@ -54,6 +54,22 @@ const updateDistributor = async (req, res) => {
   }
 };
 
+// DELETE
+const deleteDistributor = async (req, res) => {
+  try {
+    const deletedDistributor = await Distributor.findByIdAndDelete(req.params.id);
+
+    if (!deletedDistributor) {
+      return res.status(404).json({ message: "Distributor not found" });
+    }
+
+    res.status(200).json({ message: "Distributor deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
 module.exports = {
   getDistributors,
   getDistributorById,
