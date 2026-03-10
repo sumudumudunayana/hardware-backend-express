586512213455559f9c073af73aa5ad71f99a8ca2
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-10 20:32:38 +0530

    routes added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 
-const productRoutes = require("./routes/productRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const companyRoutes = require("./routes/companyRoutes");
 const distributorRoutes = require("./routes/distributorRoutes");
+const itemRoutes = require("./routes/itemRoutes");
+const customerRoutes = require("./routes/customerRoutes");
+const stockRoutes = require("./routes/stockRoutes");
+const promotionRoutes = require("./routes/promotionRoutes");
+const saleRoutes = require("./routes/saleRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+
+
+
+
+
 
 const app = express();
 
@@ -18,9 +28,16 @@ app.get("/", (req, res) => {
 });
 
 // Routes
-app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/companies", companyRoutes);
 app.use("/api/distributors", distributorRoutes);
+app.use("/api/items", itemRoutes);
+app.use("/api/customers", customerRoutes);
+app.use("/api/stocks", stockRoutes);
+app.use("/api/promotions", promotionRoutes);
+app.use("/api/sales", saleRoutes);
+app.use("/api/sales", require("./routes/saleRoutes"));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/cart", cartRoutes);
 
 module.exports = app;
