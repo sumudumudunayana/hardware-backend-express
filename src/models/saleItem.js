4d8802214b0c16af836290de033723616e9cf10c
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-11 22:57:44 +0530

    sale item model updated

---
+++ src/models/saleItem.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const saleItemSchema = new mongoose.Schema(
+  {
+    saleId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Sale",
+      required: true
+    },
+
+    itemId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Item",
+      required: true
+    },
+
+    quantity: {
+      type: Number,
+      required: true
+    },
+
+    unitPrice: {
+      type: Number,
+      required: true
+    },
+
+    subtotal: {
+      type: Number,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("SaleItem", saleItemSchema);
