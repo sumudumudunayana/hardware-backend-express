6dcdcebca5bfee4a1253457283af138772ad939a
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-13 22:49:12 +0530

    category controller updated

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -58,7 +58,6 @@ const deleteCategory = async (req, res) => {
     if (!deletedCategory) {
       return res.status(404).json({ message: "Category not found" });
     }
-
     res.status(200).json({ message: "Category deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
