8ec2b11eeee550d990f111d60287c3900faf4c4c
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-28 16:19:42 +0530

    company routes updated

--- src/routes/companyRoutes.js
+++ src/routes/companyRoutes.js
@@ -9,10 +9,24 @@ const {
   deleteCompany
 } = require("../controllers/companyController");
 
+const { protect } = require("../middleware/authMiddleware");
+
+//  Protect ALL routes
+router.use(protect);
+
+//  GET all companies
 router.get("/", getCompanies);
+
+//  GET single company
 router.get("/:id", getCompanyById);
+
+//  CREATE company
 router.post("/", createCompany);
+
+//  UPDATE company
 router.put("/:id", updateCompany);
+
+//  DELETE company
 router.delete("/:id", deleteCompany);
 
 module.exports = router;
