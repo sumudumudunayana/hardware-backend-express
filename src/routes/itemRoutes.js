47f15f8626e1e3528219e25af2565c31e4b6ce63
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-28 16:20:48 +0530

    iteam routes updated

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -9,10 +9,24 @@ const {
   deleteItem
 } = require("../controllers/itemController");
 
+const { protect } = require("../middleware/authMiddleware");
+
+//  Protect ALL routes (only logged-in users)
+router.use(protect);
+
+//  GET all items
 router.get("/", getItems);
+
+//  GET single item
 router.get("/:id", getItemById);
+
+//  CREATE item
 router.post("/", createItem);
+
+//  UPDATE item
 router.put("/:id", updateItem);
+
+//  DELETE item
 router.delete("/:id", deleteItem);
 
 module.exports = router;
