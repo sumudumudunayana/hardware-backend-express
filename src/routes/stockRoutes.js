b90f8b49b87526b845473333e1b337411061f322
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-28 16:21:53 +0530

    stock routes updated

--- src/routes/stockRoutes.js
+++ src/routes/stockRoutes.js
@@ -10,11 +10,27 @@ const {
   getStockHistory
 } = require("../controllers/stockController");
 
+const { protect } = require("../middleware/authMiddleware");
+
+//  Protect ALL routes
+router.use(protect);
+
+//  GET all stock
 router.get("/", getStocks);
+
+//  CREATE stock
 router.post("/", createStock);
+
+//  UPDATE stock
 router.put("/:id", updateStock);
+
+//  DELETE stock
 router.delete("/:id", deleteStock);
+
+//  GET stock by item
 router.get("/item/:itemId", getStockByItem);
+
+//  GET stock history
 router.get("/history/:itemId", getStockHistory);
 
 module.exports = router;
