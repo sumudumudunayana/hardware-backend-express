2f079371afe1c53ab473cc3084611aa45e6a6a8f
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-28 16:21:03 +0530

    promotion routes updated

--- src/routes/promotionRoutes.js
+++ src/routes/promotionRoutes.js
@@ -9,10 +9,24 @@ const {
   deletePromotion
 } = require("../controllers/promotionController");
 
+const { protect } = require("../middleware/authMiddleware");
+
+//  Protect ALL routes
+router.use(protect);
+
+//  CREATE promotion
 router.post("/", createPromotion);
+
+//  GET all promotions
 router.get("/", getPromotions);
+
+//  GET single promotion
 router.get("/:id", getPromotionById);
+
+//  UPDATE promotion
 router.put("/:id", updatePromotion);
+
+//  DELETE promotion
 router.delete("/:id", deletePromotion);
 
 module.exports = router;
