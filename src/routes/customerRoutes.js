3debce1e298a7878e993633e54d90b6023ee5ad6
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-28 16:19:55 +0530

    customer routes updated

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -9,10 +9,24 @@ const {
   deleteCustomer
 } = require("../controllers/customerController");
 
+const { protect } = require("../middleware/authMiddleware");
+
+//  Protect ALL routes
+router.use(protect);
+
+//  GET all customers
 router.get("/", getCustomers);
+
+//  GET single customer
 router.get("/:id", getCustomerById);
+
+//  CREATE customer
 router.post("/", createCustomer);
+
+//  UPDATE customer
 router.put("/:id", updateCustomer);
+
+//  DELETE customer
 router.delete("/:id", deleteCustomer);
 
 module.exports = router;
