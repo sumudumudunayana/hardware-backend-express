eba557c0c4f6e17777ec00a52a2b8375b92a1cf9
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-28 16:21:43 +0530

    sale routes updated

--- src/routes/saleRoutes.js
+++ src/routes/saleRoutes.js
@@ -9,10 +9,24 @@ const {
   deleteSale
 } = require("../controllers/saleController");
 
+const { protect } = require("../middleware/authMiddleware");
+
+//  Protect ALL routes
+router.use(protect);
+
+//  CREATE sale
 router.post("/", createSale);
+
+//  GET all sales
 router.get("/", getAllSales);
+
+//  GET single sale
 router.get("/:id", getSaleById);
+
+//  UPDATE sale
 router.put("/:id", updateSale);
+
+//  DELETE sale
 router.delete("/:id", deleteSale);
 
 module.exports = router;
