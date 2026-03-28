130aaabfcf904a52f54acecc81c6817d1b4cf184
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-28 16:20:15 +0530

    distributor routes updated

--- src/routes/distributorRoutes.js
+++ src/routes/distributorRoutes.js
@@ -9,10 +9,24 @@ const {
   deleteDistributor
 } = require("../controllers/distributorController");
 
+const { protect } = require("../middleware/authMiddleware");
+
+//  Protect ALL routes
+router.use(protect);
+
+//  GET all distributors
 router.get("/", getDistributors);
+
+//  GET single distributor
 router.get("/:id", getDistributorById);
+
+//  CREATE distributor
 router.post("/", createDistributor);
+
+//  UPDATE distributor
 router.put("/:id", updateDistributor);
+
+//  DELETE distributor
 router.delete("/:id", deleteDistributor);
 
 module.exports = router;
