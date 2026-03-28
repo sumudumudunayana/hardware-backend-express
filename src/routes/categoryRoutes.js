2fa85f49b6a7e81f61f3e59b0ce9188c271ae3f7
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-28 16:19:32 +0530

    category routes updated

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -9,10 +9,24 @@ const {
   deleteCategory
 } = require("../controllers/categoryController");
 
+const { protect } = require("../middleware/authMiddleware");
+
+//  Protect ALL routes
+router.use(protect);
+
+//  GET all categories
 router.get("/", getCategories);
+
+//  GET single category
 router.get("/:id", getCategoryById);
+
+//  CREATE category
 router.post("/", createCategory);
+
+//  UPDATE category
 router.put("/:id", updateCategory);
+
+//  DELETE category
 router.delete("/:id", deleteCategory);
 
 module.exports = router;
