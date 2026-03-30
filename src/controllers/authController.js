728e25795595e48478284780a2e1d9df0b103a8d
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-30 22:44:58 +0530

    auth controller updated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -13,7 +13,7 @@ const register = async (req, res) => {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    // 🔍 Check existing user
+    //  Check existing user
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
