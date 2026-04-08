3e2bb9038388052b9495c94b8d0d9088ee7b3162
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-04-08 19:44:30 +0530

    auth controller updated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -19,10 +19,10 @@ const register = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // 🔐 Hash password
+    // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // 👤 Create user
+    // Create user
     const user = await User.create({
       name,
       email,
