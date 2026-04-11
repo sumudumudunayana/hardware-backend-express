643b424a959708df3940d97fd6f84f9d883bee00
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-04-11 19:34:16 +0530

    auth controler updated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,21 +7,17 @@ const jwt = require("jsonwebtoken");
 const register = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
-
     // Validate input
     if (!name || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
-
     //  Check existing user
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
-
     // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
-
     // Create user
     const user = await User.create({
       name,
