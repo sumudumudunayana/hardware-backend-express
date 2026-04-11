f0a5e9d44add88062582346dbac457ecdbf189c6
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-04-11 19:40:08 +0530

    category controller updated

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -20,15 +20,12 @@ const getCategoryById = async (req, res) => {
     const category = await Category.findById(
       req.params.id
     );
-
     if (!category) {
       return res.status(404).json({
         message: "Category not found",
       });
     }
-
     res.status(200).json(category);
-
   } catch (error) {
     res.status(500).json({
       message: error.message,
