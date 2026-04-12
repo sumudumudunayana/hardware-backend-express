e33085744b0158adbf2c2c721688a67da4368822
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-04-12 16:26:53 +0530

    customer controller updated

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -14,13 +14,11 @@ const getCustomers = async (req, res) => {
 const getCustomerById = async (req, res) => {
   try {
     const customer = await Customer.findById(req.params.id);
-
     if (!customer) {
       return res.status(404).json({
         message: "Customer not found",
       });
     }
-
     res.status(200).json(customer);
   } catch (error) {
     res.status(500).json({
