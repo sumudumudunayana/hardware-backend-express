c11f97fac658638d103525dfac825191d11fd967
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-04-13 12:13:49 +0530

    item controller updated

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -14,19 +14,17 @@ const getItems = async (req, res) => {
   }
 };
 
+
 // GET ONE ITEM
 const getItemById = async (req, res) => {
   try {
     const item = await Item.findById(req.params.id);
-
     if (!item) {
       return res.status(404).json({
         message: "Item not found",
       });
     }
-
     res.status(200).json(item);
-
   } catch (error) {
     res.status(500).json({
       message: error.message,
