e348eafda5a16a23d364578ad8e74fa90679c420
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-04-13 12:09:20 +0530

    customer controller updated

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -152,17 +152,14 @@ const deleteCustomer = async (req, res) => {
   try {
     const deletedCustomer =
       await Customer.findByIdAndDelete(req.params.id);
-
     if (!deletedCustomer) {
       return res.status(404).json({
         message: "Customer not found",
       });
     }
-
     res.status(200).json({
       message: "Customer deleted successfully",
     });
-
   } catch (error) {
     res.status(500).json({
       message: error.message,
