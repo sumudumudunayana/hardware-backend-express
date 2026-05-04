6d08e1e6dc73afde4be644269558ff2b55b32639
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-05-04 18:34:08 +0530

    auth middlewear updated

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -9,19 +9,19 @@ const protect = (req, res, next) => {
       return res.status(401).json({ message: "No token provided" });
     }
 
-    // 🔍 Check Bearer format
+    // Check Bearer format
     if (!authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ message: "Invalid token format" });
     }
 
-    // 🔑 Extract token
+    // Extract token
     const token = authHeader.split(" ")[1];
 
     if (!token) {
       return res.status(401).json({ message: "Token missing" });
     }
 
-    // 🔐 Verify token
+    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     //  Attach user info to request
